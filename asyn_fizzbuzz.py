7c3a1875e2f354ee71f43195ce01e91db7138d4a
Hellmar Becker <becker@hellmar-becker.de>
2020-06-11 10:24:41 +0200

    working async version

--- asyn_fizzbuzz.py
+++ asyn_fizzbuzz.py
@@ -1,15 +1,26 @@
 import asyncio
 
+# do something that takes delays. set global state
 async def fizzbuzz(msg, delay):
+    global state
     while True:
         print(msg)
+        state = msg
         await asyncio.sleep(delay)
+
+# use global state, no delay
+async def dottie():
+    global state
+    while True:
+        print(f"{state}.", end="", flush=True)
+        await asyncio.sleep(0)
 
 async def main():
     await asyncio.gather(
-            fizzbuzz("fizz", 3),
-            fizzbuzz("buzz", 5),
-            fizzbuzz("fizzbuzz", 15))
+            fizzbuzz("Fizz", 3),
+            fizzbuzz("Buzz", 5),
+            fizzbuzz("Fizzbuzz", 15),
+            dottie())
 
 if __name__ == '__main__':
     asyncio.run(main())
